e8e1eab3d6fa4eb1b3815506ae725c5746c07143
Gary Mann <garythemann@users.noreply.github.com>
2026-07-04 12:49:46 -0700

    Print

---
+++ pkg/automation/tasks.go
@@ -0,0 +1,30 @@
+// Copyright 2026 The Autogun Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package automation
+
+import (
+	"context"
+	"fmt"
+	"os"
+)
+
+// Print returns a Task that writes its operands to stderr, formatting them in
+// the manner of fmt.Print and appending a newline.
+func Print(args ...any) Task {
+	return TaskFunc(func(context.Context) error {
+		fmt.Fprintln(os.Stderr, args...)
+		return nil
+	})
+}
+
+// Printf returns a Task that writes a formatted message to stderr, formatting
+// it in the manner of fmt.Printf and appending a newline.
+func Printf(format string, args ...any) Task {
+	return TaskFunc(func(context.Context) error {
+		fmt.Fprintf(os.Stderr, format, args...)
+		fmt.Fprintln(os.Stderr)
+		return nil
+	})
+}
